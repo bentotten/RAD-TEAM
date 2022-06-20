9e36efc9b1eeb3c8e15e2bd50a91d85aed2af26b
Connor Baker <connorbaker01@gmail.com>
2022-06-20 15:19:10 -0400

    ppo: Update argparse configuration, various refactorings

--- algos/ppo/ppo.py
+++ algos/ppo/ppo.py
@@ -1,4 +1,6 @@
+from typing import Literal, Optional
 import numpy as np
+import numpy.typing as npt
 import torch
 from torch.optim import Adam
 import torch.nn.functional as F
@@ -33,22 +35,42 @@ class PPOBuffer:
     for calculating the advantages of state-action pairs.
     """
 
-    def __init__(self, obs_dim, act_dim, size, gamma=0.99, lam=0.90, hid_size=48):
-        self.obs_buf = np.zeros(core.combined_shape(size, obs_dim), dtype=np.float32)
-        self.act_buf = np.zeros(core.combined_shape(size), dtype=np.float32)
-        self.adv_buf = np.zeros(size, dtype=np.float32)
-        self.rew_buf = np.zeros(size, dtype=np.float32)
-        self.ret_buf = np.zeros(size, dtype=np.float32)
-        self.val_buf = np.zeros(size, dtype=np.float32)
-        self.source_tar = np.zeros((size, 2), dtype=np.float32)
-        self.logp_buf = np.zeros(size, dtype=np.float32)
-        self.obs_win = np.zeros(obs_dim, dtype=np.float32)
-        self.obs_win_std = np.zeros(obs_dim, dtype=np.float32)
+    def __init__(
+        self,
+        obs_dim: core.Shape,
+        act_dim: core.Shape,
+        size: int,
+        gamma: float = 0.99,
+        lam: float = 0.90,
+        hid_size: int = 48,
+    ):
+        self.obs_buf: npt.NDArray[np.float32] = np.zeros(
+            core.combined_shape(size, obs_dim), dtype=np.float32
+        )
+        self.act_buf: npt.NDArray[np.float32] = np.zeros(
+            core.combined_shape(size), dtype=np.float32
+        )
+        self.adv_buf: npt.NDArray[np.float32] = np.zeros(size, dtype=np.float32)
+        self.rew_buf: npt.NDArray[np.float32] = np.zeros(size, dtype=np.float32)
+        self.ret_buf: npt.NDArray[np.float32] = np.zeros(size, dtype=np.float32)
+        self.val_buf: npt.NDArray[np.float32] = np.zeros(size, dtype=np.float32)
+        self.source_tar: npt.NDArray[np.float32] = np.zeros((size, 2), dtype=np.float32)
+        self.logp_buf: npt.NDArray[np.float32] = np.zeros(size, dtype=np.float32)
+        self.obs_win: npt.NDArray[np.float32] = np.zeros(obs_dim, dtype=np.float32)
+        self.obs_win_std: npt.NDArray[np.float32] = np.zeros(obs_dim, dtype=np.float32)
         self.gamma, self.lam = gamma, lam
         self.ptr, self.path_start_idx, self.max_size = 0, 0, size
-        self.beta = 0.005
+        self.beta: float = 0.005
 
-    def store(self, obs, act, rew, val, logp, src):
+    def store(
+        self,
+        obs: npt.NDArray[np.float32],
+        act: npt.NDArray[np.float32],
+        rew: npt.NDArray[np.float32],
+        val: npt.NDArray[np.float32],
+        logp: npt.NDArray[np.float32],
+        src: npt.NDArray[np.float32],
+    ) -> None:
         """
         Append one timestep of agent-environment interaction to the buffer.
         """
@@ -61,7 +83,7 @@ class PPOBuffer:
         self.logp_buf[self.ptr] = logp
         self.ptr += 1
 
-    def finish_path(self, last_val=0):
+    def finish_path(self, last_val: int = 0) -> None:
         """
         Call this at the end of a trajectory, or when one gets cut off
         by an epoch ending. This looks back in the buffer to where the
@@ -92,7 +114,7 @@ class PPOBuffer:
 
         self.path_start_idx = self.ptr
 
-    def get(self, logger=None):
+    def get(self, logger: Optional[EpochLogger] = None) -> dict[str, torch.Tensor]:
         """
         Call this at the end of an epoch to get all of the data from
         the buffer, with advantages appropriately normalized (shifted to have
@@ -102,29 +124,32 @@ class PPOBuffer:
         self.ptr, self.path_start_idx = 0, 0
         # the next two lines implement the advantage normalization trick
         adv_mean, adv_std = mpi_statistics_scalar(self.adv_buf)
-        self.adv_buf = (self.adv_buf - adv_mean) / adv_std
+        self.adv_buf: npt.NDArray[np.float32] = (self.adv_buf - adv_mean) / adv_std
         # ret_mean, ret_std = mpi_statistics_scalar(self.ret_buf)
         # self.ret_buf = (self.ret_buf) / ret_std
         # obs_mean, obs_std = mpi_statistics_vector(self.obs_buf)
         # self.obs_buf_std_ind[:,1:] = (self.obs_buf[:,1:] - obs_mean[1:]) / (obs_std[1:])
 
+        epLens: list[int] = logger.epoch_dict["EpLen"]
+        numEps = len(epLens)
+        epLenTotal = sum(epLens)
         data = dict(
-            obs=self.obs_buf,
-            act=self.act_buf,
-            ret=self.ret_buf,
-            adv=self.adv_buf,
-            logp=self.logp_buf,
-            loc_pred=self.obs_win_std,
-            ep_len=sum(logger.epoch_dict["EpLen"]),
+            obs=torch.as_tensor(self.obs_buf, dtype=torch.float32),
+            act=torch.as_tensor(self.act_buf, dtype=torch.float32),
+            ret=torch.as_tensor(self.ret_buf, dtype=torch.float32),
+            adv=torch.as_tensor(self.adv_buf, dtype=torch.float32),
+            logp=torch.as_tensor(self.logp_buf, dtype=torch.float32),
+            loc_pred=torch.as_tensor(self.obs_win_std, dtype=torch.float32),
+            # TODO: This is unconditional so logger cannot be None
+            ep_len=torch.as_tensor(epLenTotal, dtype=torch.float32),
         )
-        data = {k: torch.as_tensor(v, dtype=torch.float32) for k, v in data.items()}
 
         if logger:
-            slice_b = 0
-            slice_f = 0
-            epLen = logger.epoch_dict["EpLen"]
             epLenSize = (
-                len(epLen) if sum(epLen) == len(self.obs_buf) else (len(epLen) + 1)
+                # If they're equal then we don't need to do anything
+                # Otherwise we need to add one to make sure that numEps is the correct size
+                numEps
+                + int(epLenTotal != len(self.obs_buf))
             )
             obs_buf = np.hstack(
                 (
@@ -136,15 +161,20 @@ class PPOBuffer:
                     self.source_tar,
                 )
             )
-            epForm = [[] for _ in range(epLenSize)]
-            for jj, ep_i in enumerate(epLen):
+            epForm: list[list[torch.Tensor]] = [[] for _ in range(epLenSize)]
+            slice_b: int = 0
+            slice_f: int = 0
+            jj: int = 0
+            # TODO: This is essentially just a sliding window over obs_buf; use a built-in function to do this
+            for ep_i in epLens:
                 slice_f += ep_i
                 epForm[jj].append(
                     torch.as_tensor(obs_buf[slice_b:slice_f], dtype=torch.float32)
                 )
                 slice_b += ep_i
+                jj += 1
             if slice_f != len(self.obs_buf):
-                epForm[jj + 1].append(
+                epForm[jj].append(
                     torch.as_tensor(obs_buf[slice_f:], dtype=torch.float32)
                 )
 
@@ -178,26 +208,26 @@ def ppo(
     dims=None,
 ):
     """
-    Proximal Policy Optimization (by clipping), 
+    Proximal Policy Optimization (by clipping),
 
     with early stopping based on approximate KL
 
-    Base code from OpenAI: 
+    Base code from OpenAI:
     https://github.com/openai/spinningup/blob/master/spinup/algos/pytorch/ppo/ppo.py
 
     Args:
         env_fn : A function which creates a copy of the environment.
             The environment must satisfy the OpenAI Gym API.
 
-        actor_critic: The constructor method for a PyTorch Module with a 
-            ``step`` method, an ``act`` method, a ``pi`` module, and a ``v`` 
-            module. The ``step`` method should accept a batch of observations 
+        actor_critic: The constructor method for a PyTorch Module with a
+            ``step`` method, an ``act`` method, a ``pi`` module, and a ``v``
+            module. The ``step`` method should accept a batch of observations
             and return:
 
             ===========  ================  ======================================
             Symbol       Shape             Description
             ===========  ================  ======================================
-            ``a``        (batch, act_dim)  | Numpy array of actions for each 
+            ``a``        (batch, act_dim)  | Numpy array of actions for each
                                            | observation.
             ``v``        (batch,)          | Numpy array of value estimates
                                            | for the provided observations.
@@ -207,7 +237,7 @@ def ppo(
 
             The ``act`` method behaves the same as ``step`` but only returns ``a``.
 
-            The ``pi`` module's forward call should accept a batch of 
+            The ``pi`` module's forward call should accept a batch of
             observations and optionally a batch of actions, and return:
 
             ===========  ================  ======================================
@@ -217,8 +247,8 @@ def ppo(
                                            | a batch of distributions describing
                                            | the policy for the provided observations.
             ``logp_a``   (batch,)          | Optional (only returned if batch of
-                                           | actions is given). Tensor containing 
-                                           | the log probability, according to 
+                                           | actions is given). Tensor containing
+                                           | the log probability, according to
                                            | the policy, of the provided actions.
                                            | If actions not given, will contain
                                            | ``None``.
@@ -231,17 +261,17 @@ def ppo(
             Symbol       Shape             Description
             ===========  ================  ======================================
             ``v``        (batch,)          | Tensor containing the value estimates
-                                           | for the provided observations. (Critical: 
+                                           | for the provided observations. (Critical:
                                            | make sure to flatten this!)
             ===========  ================  ======================================
 
 
-        ac_kwargs (dict): Any kwargs appropriate for the ActorCritic object 
+        ac_kwargs (dict): Any kwargs appropriate for the ActorCritic object
             you provided to PPO.
 
         seed (int): Seed for random number generators.
 
-        steps_per_epoch (int): Number of steps of interaction (state-action pairs) 
+        steps_per_epoch (int): Number of steps of interaction (state-action pairs)
             for the agent and the environment in each epoch.
 
         epochs (int): Number of epochs of interaction (equivalent to
@@ -250,21 +280,21 @@ def ppo(
         gamma (float): Discount factor. (Always between 0 and 1.)
 
         clip_ratio (float): Hyperparameter for clipping in the policy objective.
-            Roughly: how far can the new policy go from the old policy while 
-            still profiting (improving the objective function)? The new policy 
+            Roughly: how far can the new policy go from the old policy while
+            still profiting (improving the objective function)? The new policy
             can still go farther than the clip_ratio says, but it doesn't help
             on the objective anymore. (Usually small, 0.1 to 0.3.) Typically
-            denoted by :math:`\epsilon`. 
+            denoted by :math:`\epsilon`.
 
         pi_lr (float): Learning rate for policy optimizer.
 
         vf_lr (float): Learning rate for value function optimizer.
 
-        train_pi_iters (int): Maximum number of gradient descent steps to take 
+        train_pi_iters (int): Maximum number of gradient descent steps to take
             on policy loss per epoch. (Early stopping may cause optimizer
             to take fewer than this.)
 
-        train_v_iters (int): Number of gradient descent steps to take on 
+        train_v_iters (int): Number of gradient descent steps to take on
             value function per epoch.
 
         lam (float): Lambda for GAE-Lambda. (Always between 0 and 1,
@@ -273,7 +303,7 @@ def ppo(
         max_ep_len (int): Maximum length of trajectory / episode / rollout.
 
         target_kl (float): Roughly what KL divergence we think is appropriate
-            between new and old policies after an update. This will get used 
+            between new and old policies after an update. This will get used
             for early stopping. (Usually small, 0.01 or 0.05.)
 
         logger_kwargs (dict): Keyword args for EpochLogger.
@@ -531,12 +561,13 @@ def ppo(
                 xy_l1_particle_loss = torch.mean(l1_particle_loss)
                 l1_particle_loss = 10 * xy_l1_particle_loss
 
-                belief_loss: torch.Tensor = args["l2_weight"] * l2_particle_loss + args[
-                    "l1_weight"
-                ] * l1_particle_loss
-                total_loss: torch.Tensor = total_loss + args[
-                    "elbo_weight"
-                ] * belief_loss
+                belief_loss: torch.Tensor = (
+                    args["l2_weight"] * l2_particle_loss
+                    + args["l1_weight"] * l1_particle_loss
+                )
+                total_loss: torch.Tensor = (
+                    total_loss + args["elbo_weight"] * belief_loss
+                )
 
                 model_loss_arr = torch.hstack((model_loss_arr, total_loss.unsqueeze(0)))
 
@@ -747,20 +778,21 @@ def ppo(
 
 if __name__ == "__main__":
     import argparse
+    from rl_tools.run_utils import setup_logger_kwargs
 
     parser = argparse.ArgumentParser()
     parser.add_argument("--env", type=str, default="gym_rad_search:RadSearch-v0")
     parser.add_argument(
-        "--hid_gru", type=int, default=[24], help="A2C GRU hidden state size"
+        "--hid_gru", type=int, default=24, help="A2C GRU hidden state size"
     )
     parser.add_argument(
-        "--hid_pol", type=int, default=[32], help="Actor linear layer size"
+        "--hid_pol", type=int, default=32, help="Actor linear layer size"
     )
     parser.add_argument(
-        "--hid_val", type=int, default=[32], help="Critic linear layer size"
+        "--hid_val", type=int, default=32, help="Critic linear layer size"
     )
     parser.add_argument(
-        "--hid_rec", type=int, default=[24], help="PFGRU hidden state size"
+        "--hid_rec", type=int, default=24, help="PFGRU hidden state size"
     )
     parser.add_argument(
         "--l_pol", type=int, default=1, help="Number of layers for Actor MLP"
@@ -798,19 +830,23 @@ if __name__ == "__main__":
     )
     parser.add_argument(
         "--dims",
-        type=list,
-        default=[[0.0, 0.0], [2700.0, 0.0], [2700.0, 2700.0], [0.0, 2700.0]],
-        help="Dimensions of radiation source search area in cm, decreased by area_obs param. to ensure visilibity graph setup is valid.",
+        type=float,
+        nargs=2,
+        default=[2700.0, 2700.0],
+        metavar=("dim_length", "dim_height"),
+        help="Dimensions of radiation source search area in cm, decreased by area_obs param. to ensure visilibity graph setup is valid. Length by height.",
     )
     parser.add_argument(
         "--area_obs",
-        type=list,
+        type=float,
+        nargs=2,
         default=[200.0, 500.0],
+        metavar=("area_obs_min", "area_obs_max"),
         help="Interval for each obstruction area in cm",
     )
     parser.add_argument(
         "--obstruct",
-        type=int,
+        type=Literal[-1, 0, 1],
         default=-1,
         help="Number of obstructions present in each episode, options: -1 -> random sampling from [1,5], 0 -> no obstructions, [1-7] -> 1 to 7 obstructions",
     )
@@ -826,30 +862,24 @@ if __name__ == "__main__":
     args = parser.parse_args()
 
     # Change mini-batch size, only been tested with size of 1
-    args.batch = 1
+    batch_s: int = 1
 
     # Save directory and experiment name
-    args.env_name = "bpf"
-    args.exp_name = (
+    env_name: str = "bpf"
+    exp_name: str = (
         "loc"
-        + str(args.hid_rec[0])
+        + str(args.hid_rec)
         + "_hid"
-        + str(args.hid_gru[0])
+        + str(args.hid_gru)
         + "_pol"
-        + str(args.hid_pol[0])
+        + str(args.hid_pol)
         + "_val"
-        + str(args.hid_val[0])
+        + str(args.hid_val)
         + "_"
         + args.exp_name
         + f"_ep{args.epochs}"
         + f"_steps{args.steps_per_epoch}"
     )
-    init_dims = {
-        "bbox": args.dims,
-        "area_obs": args.area_obs,
-        "obstruct": args.obstruct,
-    }
-    max_ep_step = 120
     if args.cpu > 1:
         # max cpus, steps in batch must be greater than the max eps steps times num. of cpu
         tot_epoch_steps = args.cpu * args.steps_per_epoch
@@ -866,13 +896,24 @@ if __name__ == "__main__":
     # Generate a large random seed and random generator object for reproducibility
     robust_seed = _int_list_from_bigint(hash_seed((1 + proc_id()) * args.seed))[0]
     rng = np.random.default_rng(robust_seed)
-    init_dims["seed"] = rng
 
-    # Setup logger for tracking training metrics
-    from rl_tools.run_utils import setup_logger_kwargs
+    dim_length, dim_height = args.dims
+    area_obs_min, area_obs_max = args.area_obs
+    init_dims = {
+        "bbox": [
+            [0.0, 0.0],
+            [dim_length, 0.0],
+            [dim_length, dim_height],
+            [0.0, dim_height],
+        ],
+        "area_obs": [area_obs_min, area_obs_max],
+        "obstruct": args.obstruct,
+        "seed": rng,
+    }
+    max_ep_step = 120
 
-    logger_kwargs = setup_logger_kwargs(
-        args.exp_name, args.seed, data_dir="../../models/train", env_name=args.env_name
+    logger_kwargs: dict[str, str] = setup_logger_kwargs(
+        exp_name, args.seed, data_dir="../../models/train", env_name=env_name
     )
 
     # Run ppo training function
@@ -880,12 +921,12 @@ if __name__ == "__main__":
         lambda: gym.make(args.env, **init_dims),
         actor_critic=core.RNNModelActorCritic,
         ac_kwargs=dict(
-            hidden_sizes_pol=[args.hid_pol] * args.l_pol,
-            hidden_sizes_val=[args.hid_val] * args.l_val,
-            hidden_sizes_rec=args.hid_rec,
-            hidden=[args.hid_gru],
+            hidden_sizes_pol=[[args.hid_pol]] * args.l_pol,
+            hidden_sizes_val=[[args.hid_val]] * args.l_val,
+            hidden_sizes_rec=[args.hid_rec],
+            hidden=[[args.hid_gru]],
             net_type=args.net_type,
-            batch_s=args.batch,
+            batch_s=batch_s,
         ),
         gamma=args.gamma,
         alpha=args.alpha,
