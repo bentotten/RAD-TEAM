28489794fe3b020277c23e8ce7ba68479ee492ac
Benjamin Totten <ttotten@pdx.edu>
2023-05-07 20:49:18 -0700

    Add CUDA check

--- algos/RADTEAM/ppo.py
+++ algos/RADTEAM/ppo.py
@@ -626,6 +626,17 @@ def ppo(
 
 
 if __name__ == "__main__":
+    print("============================================================================================")
+    # set device to cpu or cuda
+    device = torch.device("cpu")
+    if torch.cuda.is_available():
+        device = torch.device("cuda:0")
+        torch.cuda.empty_cache()
+        print("Device set to : " + str(torch.cuda.get_device_name(device)))
+    else:
+        print("Device set to : cpu")
+    print("============================================================================================")
+
     import argparse
 
     parser = argparse.ArgumentParser()
