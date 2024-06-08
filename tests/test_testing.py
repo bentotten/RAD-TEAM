e52c0761c9473b35b6ee96a69c41d91f3a36dab9
Ben Totten <ttotten@pdx.edu>
2024-06-08 13:41:15 -0700

    Testing linting/typing done

--- tests/test_testing.py
+++ tests/test_testing.py
@@ -3,18 +3,8 @@ import pytest
 
 @pytest.mark.happy
 class Test_Happycase:
-    def missing_type_defs(self, test) -> int:
-        return test
-
-    def missing_return_defs(self, test: int):
-        return test
-
-    def fetch_an_int(self, im_an_int: int) -> int:
-        return im_an_int
-
     def test_typing(self) -> None:
-        im_an_int = self.fetch_an_int("clearly not an int")
-        assert im_an_int == im_an_int
+        assert 1 == 1
 
     def test_failed_test(self) -> None:
         assert 1 == 0
